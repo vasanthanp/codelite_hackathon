fc01225dd2799e09b0778dcbb7f4c280bf60dbe4
vasanthanp <vasanthan322001@gmail.com>
2021-03-29 11:34:17 +0000

    Added spring folder structure

---
+++ spring/src/main/java/com/example/Model/UserModel.java
@@ -0,0 +1,9 @@
+class UserModel{
+    private String email;
+    private String password;
+    private String username;
+    private String mobileNumber;
+    private int age;
+    private String userRole;
+
+}
