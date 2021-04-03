8cb34053f4e4e424a0d97143bb362ca6cb881b8e
Rajesh Kumar <45392649+rajeshkumarveer@users.noreply.github.com>
2021-04-03 11:56:27 +0530

    Delete spring/src/main/java/com/example/Controller directory

--- spring/src/main/java/com/example/Controller/AdminController.java
+++
@@ -1,9 +0,0 @@
-package com.example.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AdminController {
-    // @PutMapping("/admin/edit")
-    // public AdminModel editAdmin()
-}
